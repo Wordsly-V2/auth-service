c0c70fb3a54d4de5b50921ee40bc243dcc289791
Dinh Thanh <dinhthanh11011@gmail.com>
2026-01-19 15:49:07 +0700

    base login flow with jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoginModule } from '@/login/login.module';
 import { PrismaModule } from '@/prisma/prisma.module';
 import configuration from '@/config/configuration';
+import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -12,6 +13,27 @@ import configuration from '@/config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const secret = config.get('jwt.secret') as string;
+        const expiresIn = config.get(
+          'jwt.expiresIn',
+        ) as JwtSignOptions['expiresIn'];
+
+        return {
+          secret,
+          signOptions: {
+            expiresIn: expiresIn,
+            algorithm: 'RS256',
+            issuer: 'auth-service',
+          },
+        };
+      },
+    }),
+
     LoginModule,
     PrismaModule,
   ],
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,92 +1,66 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@/prisma/prisma.service';
 import { IOAuthLoginResponseDTO, IOAuthUserDTO } from '@/login/DTO/login.DTO';
-import { PrismaClient, User, UserLogin } from '@prisma/client';
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserLogin } from '@prisma/client';
 import { v7 as uuidv7 } from 'uuid';
 
 @Injectable()
 export class LoginService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async handleOAuthLogin(
     userPayload: IOAuthUserDTO,
   ): Promise<IOAuthLoginResponseDTO> {
     let userLogin: UserLogin | null = null;
-    let user: User | null = null;
-    const now = new Date();
 
-    await this.prismaService.$transaction(async (tx: PrismaClient) => {
-      const existingUserLogin = await tx.userLogin.findFirst({
+    return this.prismaService.$transaction(async (transaction) => {
+      userLogin = await transaction.userLogin.findUnique({
         where: {
-          OR: [
-            { googleId: userPayload.id },
-            { facebookId: userPayload.id },
-            { gmail: userPayload.email },
-          ],
+          providerUserId: userPayload.id,
         },
       });
 
-      switch (userPayload.provider) {
-        case 'google': {
-          userLogin = await tx.userLogin.upsert({
-            where: {
-              id: existingUserLogin?.id ?? '0',
-            },
-            update: { gmail: userPayload.email, googleId: userPayload.id },
-            create: {
-              gmail: userPayload.email,
-              googleId: userPayload.id,
-              id: uuidv7(),
-              createdAt: now,
-              updatedAt: now,
-            },
-          });
-          break;
-        }
-        case 'facebook': {
-          userLogin = await tx.userLogin.upsert({
-            where: {
-              id: existingUserLogin?.id ?? '0',
-            },
-            update: { gmail: userPayload.email, facebookId: userPayload.id },
-            create: {
-              gmail: userPayload.email,
-              facebookId: userPayload.id,
-              id: uuidv7(),
-              createdAt: now,
-              updatedAt: now,
-            },
-          });
-          break;
-        }
+      if (!userLogin) {
+        userLogin = await transaction.userLogin.create({
+          data: {
+            id: uuidv7(),
+            providerUserId: userPayload.id,
+            provider: userPayload.provider,
+            status: 'active',
+          },
+        });
       }
 
-      user = await tx.user.upsert({
+      await transaction.user.upsert({
         where: {
           userLoginId: userLogin.id,
-        },
-        update: {
-          displayName: userPayload.displayName,
-          pictureUrl: userPayload.picture,
-          updatedAt: now,
         },
         create: {
           id: uuidv7(),
           userLoginId: userLogin.id,
+          gmail: userPayload.email,
           displayName: userPayload.displayName,
           pictureUrl: userPayload.picture,
-          createdAt: now,
-          updatedAt: now,
+        },
+        update: {
+          gmail: userPayload.email,
+          displayName: userPayload.displayName,
+          pictureUrl: userPayload.picture,
         },
       });
+
+      const accessToken = await this.jwtService.signAsync({
+        userLoginId: userLogin.id,
+      });
+
+      return {
+        accessToken: accessToken,
+        refreshToken: 'refreshToken',
+      };
     });
-
-    console.log(user);
-    console.log(userLogin);
-
-    return {
-      accessToken: 'accessToken',
-      refreshToken: 'refreshToken',
-    };
   }
 }
