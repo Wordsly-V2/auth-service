a8c2522f72bc9330e0655b2db77200ccad619f74
Dinh Thanh <dinhthanh11011@gmail.com>
2026-01-27 23:40:48 +0700

    add tsconfig-paths on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'tsconfig-paths/register';
+
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
