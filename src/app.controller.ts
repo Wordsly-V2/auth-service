0d34a39a0edc52315d3a8aea85e50617472aeb56
Dinh Thanh <dinhthanh11011@gmail.com>
2026-02-01 16:16:23 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  getHealthHttp(): string {
+  getHealth(): string {
     return this.appService.getHealth();
   }
 }
