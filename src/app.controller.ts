4d019dfa07951789d073f678d93855571965e890
Dinh Thanh <dinhthanh11011@gmail.com>
2026-06-13 19:19:40 +0700

    add health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,11 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get('ping')
+    ping(): string {
+        return this.appService.getHealth();
+    }
+
+    @Get('health')
     getHealth(): string {
         return this.appService.getHealth();
     }
